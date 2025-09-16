ad00b4322acbb1bd324e101668375b2d09795eaa
Safal Karki <82864764+SafalKarkey@users.noreply.github.com>
2025-09-16 07:26:07 +0545

    Implement sendMessageToActiveTab function

--- Week 1 and 2/Assignments/sink/popup.js
+++ Week 1 and 2/Assignments/sink/popup.js
@@ -1,7 +1,14 @@
-// Simple popup script
+function sendMessageToActiveTab(message) {
+  const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+  const response = await chrome.tabs.sendMessage(tab.id, message);
+  // TODO: Do something with the response.
+}
+
+//console.log(`Current tab: chrome.tabs.currentWindow`)
 document.addEventListener('DOMContentLoaded', function() {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {}, function(response) {
+      console.log(response);
       var badList = response.foundBad;
       var html = '';
       if (badList.length > 0) {
